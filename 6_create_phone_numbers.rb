9202048807168b9127a7b1ca259c2fe95a11e222
ejaramillom <ejaramilo@hotmail.com>
2024-07-23 16:45:12 +0000

    6 create phone numbers

--- 6_create_phone_numbers.rb
+++ 6_create_phone_numbers.rb
@@ -1,6 +1,6 @@
 def create_phone_number(numbers)
-  p numbers.join.insert(0, '(').insert(4, ')').insert(5, ' ').insert(9, '-')
-  # '(%d%d%d) %d%d%d-%d%d%d%d' % array
+  # p numbers.join.insert(0, '(').insert(4, ')').insert(5, ' ').insert(9, '-')
+  p '(%d%d%d) %d%d%d-%d%d%d%d' % numbers
 end
 
 phone = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
