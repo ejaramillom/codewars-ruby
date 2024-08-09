6c3c7df3ac9c40b57e14f84cca4e0dd0a54e1e24
ejaramillom <ejaramilo@hotmail.com>
2024-08-09 15:44:55 +0000

    9 roman numbers

--- 7_longest_flatten_string.rb
+++ 7_longest_flatten_string.rb
@@ -1,5 +1,5 @@
 def longest(a1, a2)
-  (a1 + a2).split("").uniq.sort.join 
+  (a1 + a2).chars.uniq.sort.join 
 end
 
 a = "xyaabbbccccdefww"
--- main.rb
+++ main.rb
@@ -1,1 +1,16 @@
-puts 'Hello, World!'
+# callbacks
+
+class Style
+  before_save :upcase_name
+  after_save :success_register_email
+
+  private
+
+  def upcase_name
+    self.name.upcase
+  end   
+
+  def success_register_email
+    UserMailer.welcome_email(self).deliver_now
+  end  
+end  
